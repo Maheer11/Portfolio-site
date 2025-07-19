e5955157681ecd2e64d2ddac453de27a10a9a850
Maheer11 <117725925+Maheer11@users.noreply.github.com>
2025-07-19 20:34:53 +0100

    add cv

--- portfolio.js
+++ portfolio.js
@@ -5,3 +5,15 @@ const navLinks = document.getElementById('navlinks');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  });
+
+  document.querySelectorAll('.procards').forEach(card => {
+    observer.observe(card);
+  });
